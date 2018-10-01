3e952b468ed9238c4040256a4779c24499b17886
raylison100 <raylison100@gmail.com>
2018-09-30 22:28:37 -0300

    entidade estoque

--- src/main/java/br/com/demo/model/entity/Estoque.java
+++ src/main/java/br/com/demo/model/entity/Estoque.java
@@ -1,10 +1,16 @@
 package br.com.demo.model.entity;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name="estoque")
 public class Estoque {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private Produto produto;
     private long qtd;
     private List<Debito> debitos;
